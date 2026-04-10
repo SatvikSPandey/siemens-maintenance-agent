45e105053fffa3a594e41d03affb1c1b85b6dd4a
SatvikSPandey <satvikpan@gmail.com>
2026-04-10 14:28:08 +0530

    Fix Cohere model name

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ CHECKPOINT_DB_PATH = DATA_DIR / "checkpoints.db"
 OLLAMA_MODEL = "llama3"
 OLLAMA_BASE_URL = "http://localhost:11434"
 
-COHERE_LLM_MODEL = "command-r"
+COHERE_LLM_MODEL = "command-r-plus"
 
 import socket
 def is_ollama_running():
